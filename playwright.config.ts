2881cbde8e995048b52699ecc5ebfdf10f7162cd
Nikhil <nikhilgangwar23@gmail.com>
2024-05-18 21:42:12 +1000

    Changes in configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -54,9 +54,9 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://www.saucedemo.com",
-
+    screenshot: "on",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
   },
 
   /* Configure projects for major browsers */
